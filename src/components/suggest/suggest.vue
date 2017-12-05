7b34623d8acd92154be1f836d113f20a3c47f275
flsfls <947127871@qq.com>
2017-12-05 23:52:06 +0800

    'search_1'

---
+++ src/components/suggest/suggest.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="suggest">
+        <ul class="suggest-list">
+            <li @click="selectItem(item)" class="suggest-item" v-for="item in result">
+                <div class="icon">
+                    <i :class="getIconCls(item)"></i>
+                </div>
+                <div class="name">
+                    <p class="text" v-html="getDisplayName(item)"></p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { search } from 'api/search'
+import { ERR_OK } from 'api/config'
+import { filterSinger } from 'common/js/song'
+
+const TYPE_SINGER = 'singer'
+const perpage = 20
+
+
+export default {
+    props: {
+        query: {
+            type: String,
+            default: ''
+        },
+        showSinger: {
+            type: Boolean,
+            default: true
+        }
+    },
+    watch: {
+        query() {
+            this._search()
+        }
+    },
+    data() {
+        return {
+            result: [],
+            page: 1
+        }
+    },
+    methods: {
+        _search() {
+            search(this.query, this.page, this.showSinger, perpage).then(res => {
+                if (res.code === ERR_OK) {
+                    this.result = this.normalizeData(res.data)
+                    console.log("result", this.result)
+                }
+            })
+        },
+        normalizeData(data) {
+            let ret = []
+            if (data.zhida && data.zhida.singerid) {
+                ret.push({ ...data.zhida, ...{ type: TYPE_SINGER } })
+            }
+            return ret = ret.concat(data.song.list)
+        },
+        getIconCls(item) {
+            if (item.type === TYPE_SINGER) {
+                return 'icon-mine'
+            } else {
+                return 'icon-music'
+            }
+        },
+        getDisplayName(item) {
+            if(item.type === TYPE_SINGER) {
+                return `${item.singername}`
+            }else {
+                return `${item.songname}-${filterSinger(item.singer)}`
+            }
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .suggest
+    height: 100%
+    overflow: hidden
+    .suggest-list
+      padding: 0 30px
+      .suggest-item
+        display: flex
+        align-items: center
+        padding-bottom: 20px
+      .icon
+        flex: 0 0 30px
+        width: 30px
+        [class^="icon-"]
+          font-size: 14px
+          color: $color-text-d
+      .name
+        flex: 1
+        font-size: $font-size-medium
+        color: $color-text-d
+        overflow: hidden
+        .text
+          no-wrap()
+    .no-result-wrapper
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
+</style>
